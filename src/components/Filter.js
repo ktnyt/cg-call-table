58adfe2c83a1f35559e4b31b93ea028f11e470f8
ktnyt <kotone@sfc.keio.ac.jp>
2017-08-05 03:02:12 +0900

    Add unit filtering functionality

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,15 +1,42 @@
-import React from 'react'
-import { Endpoint } from 'react-rest'
-import { mapToProp } from 'utils'
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import { Endpoints } from 'react-rest'
+import { flatten, mapToProp } from 'utils'
 
 import DebouncedInput from './DebouncedInput'
 
-const Filter = ({ onChange, placeholder='', style={} }) => (
-  <Endpoint name='characters' path='characters' render={({ characters }) => (
-      <DebouncedInput type='text' delay={100} placeholder={placeholder} style={style} onChange={
-          ({ target: { value: search } }) => search.length ? characters.handlers.browse({ search }).then(mapToProp('id')).then(onChange) : onChange([])
-      } />
-    )} />
-)
+class Filter extends Component {
+  state = { data: [] }
+
+  static propTypes = {
+    onChange: PropTypes.func,
+  }
+
+  static defaultProps = {
+    onChange: event => event,
+  }
+
+  render = () => {
+    const { onChange, ...props } = this.props
+
+    return (
+      <Endpoints specs={{
+          characters: { path: 'characters' },
+          units: { path: 'units' },
+        }} render={({ characters, units }) => {
+          const handleChange = ({ target: { value: search } }) => {
+            if(!search.length) onChange([])
+            Promise.all([
+              characters.handlers.browse({ search }).then(mapToProp('id')),
+              units.handlers.browse({ search }).then(mapToProp('members')).then(flatten),
+            ]).then(flatten).then(onChange)
+          }
+          return (
+            <DebouncedInput type='text' delay={100} {...props} onChange={handleChange} />
+          )
+        }} />
+    )
+  }
+}
 
 export default Filter
