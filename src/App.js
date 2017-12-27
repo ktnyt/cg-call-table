2d3bb7880eba8cd102073f74ef34886e01d416fe
Kotone Itaya <kotone@sfc.keio.ac.jp>
2017-12-27 17:17:43 +0900

    Refactor to new architecture

--- src/App.js
+++ src/App.js
@@ -1,30 +1,13 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
 import { ThemeProvider } from 'react-jss'
-import { Client, Endpoint, middleware } from 'react-rest-client'
-import { List, WindowScroller } from 'react-virtualized'
 
-import Col from './components/Col'
+import Filters from './components/Filters'
+import Table from './components/Table'
 
-const width = 800
-const cellHeight= 30
-
-const styles = {
-  row: {
-    width,
-  },
-  col: {
-    float: 'left',
-    width: width / 4,
-    fontSize: 12,
-  },
-  input: {
-    border: 'none',
-    width: '100%',
-  }
-}
+import handlers from './handlers'
 
 const theme = {
-  color: {
+  colors: {
     cute: ['#f8adcc', '#fcc9de'],
     cool: ['#9ab3ff', '#ccdaff'],
     pasn: ['#fee3b3', '#feeccc'],
@@ -33,92 +16,90 @@ const theme = {
   cell: {
     padding: 3,
     width: 200,
-    height: 24,
+    height: 22,
   },
 }
 
-const CalltableItem = ({ caller, callee, called, remark, onSubmit }) => (
-  <div style={styles.row}>
-    <Col type={caller.type}>{caller.name}</Col>
-    <Col type={callee.type}>{callee.name}</Col>
-    <Col type={callee.type}>{called}</Col>
-    <Col type={callee.type}>{remark}</Col>
-  </div>
-)
-
-class Frame extends Component {
-  render = () => (
-    <Client base='https://api.imascg.moe' middleware={[middleware.handleJson()]}>
-      <React.Fragment>
-        <Endpoint path='characters'>
-          <Endpoint path='readings'>
-            {null}
-          </Endpoint>
-        </Endpoint>
-        <Endpoint path='units'>
-          <Endpoint path='readings'>
-            {null}
-          </Endpoint>
-        </Endpoint>
-        <Endpoint path='calltable'>
-          {null}
-        </Endpoint>
-      </React.Fragment>
-    </Client>
-  )
-}
-
 class App extends Component {
-  state = { caller: '', callee: '', called: '' }
+  state = {
+    characters: false,
+    character_readings: false,
+    calltable: false,
+    caller: '',
+    callee: '',
+    called: '',
+  }
 
-  render = () => (
-    <ThemeProvider theme={theme}>
-      <Client base='https://api.imascg.moe' middleware={[middleware.handleJson()]}>
-        <Endpoint path='characters' render={characters => (
-          <div style={{ width, margin: 'auto' }}>
-            <Frame />
-            <Endpoint path='readings' render={readings => readings.response ? (
-              <div></div>
-            ) : null} />
-            <Endpoint path='/calltable' options={this.state} render={calltable => {
-              if(!characters.response || !calltable.response) {
-                return null
-              }
+  componentWillMount = () => {
+    this.unbind_characters = handlers.characters.bind(data => {
+      const characters = data.map(({ id, ...rest }) => ({ [id]: rest })).reduce((prev, next) => ({ ...prev, ...next }))
+      this.setState({ characters })
+    })
+    handlers.characters.browse()
 
-              const mapped = calltable.response.data.map(entry => ({
-                ...entry,
-                caller: characters.response.data.find(character => character.id === entry.caller),
-                callee: characters.response.data.find(character => character.id === entry.callee),
-              }))
+    this.unbind_character_readings = handlers.character_readings.bind(character_readings => this.setState({ character_readings }))
+    handlers.character_readings.browse()
 
-              return (
-                <WindowScroller>
-                {({ height, scrollTop }) => (
-                  <List
-                    autoHeight
-                    width={width}
-                    height={height}
-                    scrollTop={scrollTop}
-                    rowCount={mapped.length}
-                    rowHeight={theme.cell.height}
-                    rowRenderer={({ index, key, style }) => (({ caller, callee, called, remark }, even) => (
-                      <div key={key} style={style}>
-                        <Col type={caller.type} even={even}>{caller.name}</Col>
-                        <Col type={callee.type} even={even}>{callee.name}</Col>
-                        <Col type={callee.type} even={even}>{called}</Col>
-                        <Col type={callee.type} even={even}>{remark}</Col>
-                      </div>
-                    ))(mapped[index], index % 2 === 0)}
-                  />
-                )}
-                </WindowScroller>
-              )
-            }} />
+    this.unbind_calltable = handlers.calltable.bind(calltable => this.setState({ calltable }))
+    handlers.calltable.browse()
+  }
+
+  componentWillUnmount = () => {
+    this.unbind_characters()
+    this.unbind_character_readings()
+    this.unbind_calltable()
+  }
+
+  render = () => {
+    const {
+      characters,
+      character_readings,
+      calltable,
+      caller,
+      callee,
+      called,
+    } = this.state
+
+    const ready = !!characters && !!character_readings && !!calltable
+
+    if(!ready) return null
+
+    const callers = character_readings.filter(entry => entry.reading.match(caller)).map(entry => entry.id)
+    const callees = character_readings.filter(entry => entry.reading.match(callee)).map(entry => entry.id)
+
+    const filtered = calltable
+    .filter(entry => callers.includes(entry.caller))
+    .filter(entry => callees.includes(entry.callee))
+    .filter(entry => entry.called.match(called))
+    .map(({ id, caller, callee, called, remark }) => ({
+      id,
+      caller: characters[caller],
+      callee: characters[callee],
+      called,
+      remark,
+    }))
+
+    const changeCaller = event => this.setState({ caller: event.target.value })
+    const changeCallee = event => this.setState({ callee: event.target.value })
+    const changeCalled = event => this.setState({ called: event.target.value })
+
+    return (
+      <ThemeProvider theme={theme}>
+        <Fragment>
+          <div>
+            <Filters
+              onChangeCaller={changeCaller}
+              onChangeCallee={changeCallee}
+              onChangeCalled={changeCalled}
+            />
           </div>
-        )} />
-      </Client>
-    </ThemeProvider>
-  )
+          <div>
+            <Table table={filtered} />
+          </div>
+        </Fragment>
+      </ThemeProvider>
+    )
+  }
 }
 
 export default App
